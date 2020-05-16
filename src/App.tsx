889028660ace197d2b3206e25c4bf49f9bdb3926
George Tavantzopoulos <g.tavantzopoulos@gmail.com>
2020-05-16 16:01:48 +0300

    Switch in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
 import { ApolloProvider } from '@apollo/client';
-
+import React from 'react';
 import client from './apollo';
+import './App.css';
+
 
 function App() {
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={ client }>
       <div className="App">Hello to slender github explorer</div>
     </ApolloProvider>
   );
